770a2dc1de1b34bbf8b2d89459fb274c6977bf5a
MYy-program <1758336685@qq.com>
2021-03-26 00:16:40 +0800

    提交作业4

---
+++ HomeWork4/HomeWork4-2/Clock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+namespace HomeWork4_2
+{
+    class Clock
+    {
+        internal void ShowCurrentTime(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine(DateTime.Now.ToString());
+        }
+
+        internal void BellRing(object sender, ElapsedEventArgs e)
+        {
+            if (DateTime.Now.ToString() == "2021/3/25 23:23:00")
+            {
+                Console.WriteLine("响铃");
+
+            }
+        }
+    }
+}
---
+++ HomeWork4/HomeWork4-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Timers;
+namespace HomeWork4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            Clock clock = new Clock();
+            timer.Elapsed += clock.ShowCurrentTime;
+            timer.Elapsed += clock.BellRing;
+            timer.Start();
+            Console.ReadLine();
+        }
+    }
+}

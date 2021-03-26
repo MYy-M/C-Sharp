a382d9f18ab35ca74acb5c5f29b637eaa36c7caa
MYy-program <1758336685@qq.com>
2021-03-26 08:45:42 +0800

    提交HomeWork4

--- HomeWork4/HomeWork4-2/Clock.cs
+++ HomeWork4/HomeWork4-2/Clock.cs
@@ -8,14 +8,15 @@ namespace HomeWork4_2
     {
         internal void ShowCurrentTime(object sender, ElapsedEventArgs e)
         {
-            Console.WriteLine(DateTime.Now.ToString());
+            Console.Write(DateTime.Now.ToString());
+            Console.WriteLine("  时钟滴答");
         }
 
         internal void BellRing(object sender, ElapsedEventArgs e)
         {
             if (DateTime.Now.ToString() == "2021/3/25 23:23:00")
             {
-                Console.WriteLine("响铃");
+                Console.WriteLine("闹钟响铃！");
 
             }
         }
---
+++ HomeWork4/HomeWork4/GenericList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork4_1
+{
+    class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            Node<T> node = head;
+            while (node != tail && head != null)
+            {
+                action(node.Data);
+                node = node.Next;
+            }
+        }
+
+    }
+}

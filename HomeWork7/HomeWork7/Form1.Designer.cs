95fc8aa97d6f447323331d6bac7706fb67d3be20
MYy-program <1758336685@qq.com>
2021-04-15 22:17:02 +0800

    提交作业7

---
+++ HomeWork7/HomeWork7/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace HomeWork7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.thR = new System.Windows.Forms.TextBox();
+            this.n = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.perL = new System.Windows.Forms.TextBox();
+            this.leng = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.perR = new System.Windows.Forms.TextBox();
+            this.thL = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.draw = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Controls.Add(this.draw);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(529, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(507, 564);
+            this.panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 232F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.thR, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.n, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.perL, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.leng, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.perR, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.thL, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 6);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 44F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 43F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(473, 297);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("华文新魏", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(3, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "递归深度（n）：";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("华文新魏", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.Location = new System.Drawing.Point(3, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(209, 21);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "左分支角度（th2）：";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("华文新魏", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label5.Location = new System.Drawing.Point(3, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(198, 21);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "左分支长度比(per2):";
+            // 
+            // thR
+            // 
+            this.thR.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.thR.Location = new System.Drawing.Point(244, 175);
+            this.thR.Name = "thR";
+            this.thR.Size = new System.Drawing.Size(169, 28);
+            this.thR.TabIndex = 6;
+            this.thR.TextChanged += new System.EventHandler(this.thR_TextChanged);
+            // 
+            // n
+            // 
+            this.n.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.n.Location = new System.Drawing.Point(244, 8);
+            this.n.Name = "n";
+            this.n.Size = new System.Drawing.Size(169, 28);
+            this.n.TabIndex = 1;
+            this.n.TextChanged += new System.EventHandler(this.n_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("华文新魏", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label6.Location = new System.Drawing.Point(3, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(209, 21);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "右分支长度比(per1)：";
+            // 
+            // perL
+            // 
+            this.perL.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.perL.Location = new System.Drawing.Point(244, 133);
+            this.perL.Name = "perL";
+            this.perL.Size = new System.Drawing.Size(169, 28);
+            this.perL.TabIndex = 7;
+            this.perL.TextChanged += new System.EventHandler(this.perL_TextChanged);
+            // 
+            // leng
+            // 
+            this.leng.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.leng.Location = new System.Drawing.Point(244, 52);
+            this.leng.Name = "leng";
+            this.leng.Size = new System.Drawing.Size(169, 28);
+            this.leng.TabIndex = 4;
+            this.leng.TextChanged += new System.EventHandler(this.leng_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("华文新魏", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.Location = new System.Drawing.Point(3, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "主干长度(leng)：";
+            // 
+            // perR
+            // 
+            this.perR.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.perR.Location = new System.Drawing.Point(244, 93);
+            this.perR.Name = "perR";
+            this.perR.Size = new System.Drawing.Size(169, 28);
+            this.perR.TabIndex = 8;
+            this.perR.TextChanged += new System.EventHandler(this.perR_TextChanged);
+            // 
+            // thL
+            // 
+            this.thL.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.thL.Location = new System.Drawing.Point(244, 218);
+            this.thL.Name = "thL";
+            this.thL.Size = new System.Drawing.Size(169, 28);
+            this.thL.TabIndex = 5;
+            this.thL.TextChanged += new System.EventHandler(this.thL_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("华文新魏", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label7.Location = new System.Drawing.Point(3, 264);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(191, 21);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "画笔颜色（pen）：";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("华文新魏", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label4.Location = new System.Drawing.Point(3, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(190, 21);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "右分支角度（th1）:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 38);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "选择颜色";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // draw
+            // 
+            this.draw.Location = new System.Drawing.Point(401, 374);
+            this.draw.Name = "draw";
+            this.draw.Size = new System.Drawing.Size(75, 33);
+            this.draw.TabIndex = 0;
+            this.draw.Text = "draw\r\n";
+            this.draw.UseVisualStyleBackColor = true;
+            this.draw.Click += new System.EventHandler(this.draw_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(529, 564);
+            this.panel2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1036, 564);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button draw;
+        private System.Windows.Forms.TextBox n;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox perR;
+        private System.Windows.Forms.TextBox perL;
+        private System.Windows.Forms.TextBox thR;
+        private System.Windows.Forms.TextBox thL;
+        private System.Windows.Forms.TextBox leng;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ HomeWork7/HomeWork7/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private Graphics graphics;
+
+        int num = 0;
+        double length = 0;
+        Pen pen;
+        double th1 = 30 * Math.PI / 180;
+        double th2 = 20 * Math.PI / 180;
+        double per1 = 0.6;
+        double per2 = 0.7;
+
+        private void n_TextChanged(object sender, EventArgs e)
+        {
+            num = Convert.ToInt32(n.Text);
+        }
+
+        private void leng_TextChanged(object sender, EventArgs e)
+        {
+            length = Convert.ToInt32(leng.Text);
+        }
+
+        private void perR_TextChanged(object sender, EventArgs e)
+        {
+            per1= Convert.ToDouble(perR.Text);
+        }
+
+        private void perL_TextChanged(object sender, EventArgs e)
+        {
+            per2 = Convert.ToDouble(perL.Text);
+        }
+
+        private void thR_TextChanged(object sender, EventArgs e)
+        {
+            th1 = Convert.ToDouble(thR.Text);
+        }
+
+        private void thL_TextChanged(object sender, EventArgs e)
+        {
+            th2 = Convert.ToDouble(thL.Text);
+        }
+
+        private void Colour_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void draw_Click(object sender, EventArgs e)
+        {
+
+            if (graphics == null) 
+                graphics = panel2.CreateGraphics();
+            drawCayleyTree(num, 150, 200, length, -Math.PI / 2);
+        }
+        
+       
+
+        void drawCayleyTree(int n, double x0, double y0, double leng, double th)
+        {
+
+            if (n == 0) return;
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+            drawLine(x0, y0, x1, y1);
+
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x1, y1, per2 * leng, th - th2);
+        }
+
+        void drawLine(double x0, double y0,double x1, double y1)
+        {
+            graphics.DrawLine(pen,(int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            Color color = colorDialog1.Color;
+            pen = new Pen(color);
+        }
+    }
+}
